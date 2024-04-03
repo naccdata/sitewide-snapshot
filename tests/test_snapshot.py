955b8b42bb1f147fa116c1463aa92828798dd9b2
David Parker <davidparker@flywheel.io>
2024-04-03 16:32:23 -0500

    isort

--- tests/test_snapshot.py
+++ tests/test_snapshot.py
@@ -2,19 +2,10 @@ from unittest.mock import MagicMock, patch
 
 from fw_gear_sitewide_snapshot.snapshot import snapshot
 
-from .snapshot_assets import (
-    FAKE_BATCH_NAME,
-    FAKE_DATE,
-    FAKE_GROUP,
-    FAKE_KEY,
-    FAKE_PROJECT_ID,
-    FAKE_PROJECT_LABEL,
-    FAKE_RESPONSE,
-    FAKE_SNAPSHOT_ID,
-    mock_client,
-    mock_project,
-    mock_sdk_client,
-)
+from .snapshot_assets import (FAKE_BATCH_NAME, FAKE_DATE, FAKE_GROUP, FAKE_KEY,
+                              FAKE_PROJECT_ID, FAKE_PROJECT_LABEL,
+                              FAKE_RESPONSE, FAKE_SNAPSHOT_ID, mock_client,
+                              mock_project, mock_sdk_client)
 
 
 @patch("fw_client.FWClient")
